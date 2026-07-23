c0e5320b69630af23022fbfe14ae596538f67b8f
bayresul4224-alt <bayresul4224@gmail.com>
2026-07-23 22:35:17 +0300

    Add SoundManager class with sound playback methods

---
+++ .github/SoundManager.java
@@ -0,0 +1,13 @@
+package com.ragequit.game;
+
+public class SoundManager {
+
+    public void playJumpSound() {
+    }
+
+    public void playDeathSound() {
+    }
+
+    public void playFinishSound() {
+    }
+}
