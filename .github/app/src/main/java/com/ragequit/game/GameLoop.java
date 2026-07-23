c1fcc71897ab99c69d62d156867d9fd07f6e696a
bayresul4224-alt <bayresul4224@gmail.com>
2026-07-23 18:02:51 +0300

    Implement basic GameLoop class with start/stop methods

---
+++ .github/app/src/main/java/com/ragequit/game/GameLoop.java
@@ -0,0 +1,18 @@
+package com.ragequit.game;
+
+public class GameLoop {
+
+    private boolean running;
+
+    public void start() {
+        running = true;
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+}
