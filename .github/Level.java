873b9f7df9a3c4293de3a5148bf1be3731f4c5b4
bayresul4224-alt <bayresul4224@gmail.com>
2026-07-23 22:33:32 +0300

    Add Level class for game level representation

---
+++ .github/Level.java
@@ -0,0 +1,14 @@
+package com.ragequit.game;
+
+public class Level {
+
+    private final int levelNumber;
+
+    public Level(int levelNumber) {
+        this.levelNumber = levelNumber;
+    }
+
+    public int getLevelNumber() {
+        return levelNumber;
+    }
+}
